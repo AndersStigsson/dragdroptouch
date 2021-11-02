e6cc8dc9b19b6a045805f238e90305db5ba3c952
Anders Stigsson <anders@pixelstore.se>
2021-11-02 09:35:24 +0100

    Testing scrolling

--- DragDropTouch.js
+++ DragDropTouch.js
@@ -231,7 +231,7 @@ var DragDropTouch;
                 // continue dragging
                 if (this._img) {
                     this._lastTouch = e;
-                    //e.preventDefault(); // prevent scrolling
+                    e.preventDefault(); // prevent scrolling
                     this._dispatchEvent(e, 'drag', this._dragSource);
                     if (target != this._lastTarget) {
                         this._dispatchEvent(this._lastTouch, 'dragleave', this._lastTarget);
@@ -372,6 +372,10 @@ var DragDropTouch;
                     s.zIndex = '999999';
                     s.left = Math.round(pt.x - _this._imgOffset.x) + 'px';
                     s.top = Math.round(pt.y - _this._imgOffset.y) + 'px';
+			console.log(window.innerHeight);
+		if(window.innerHeight - s.top < 30) {
+			window.scroll(0, window.scrollY + 30)
+		}
                 }
             });
         };
